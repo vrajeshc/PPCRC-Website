4a101e63d4c23900226bd877b40e59a2ab5afbd7
Nikhil Kothari <nik.kothari22@live.com>
2020-05-08 11:04:58 +0530

    initial commit

---
+++ footer.php
@@ -0,0 +1,59 @@
+<?php echo '
+<header>
+<footer>
+<div class=" footer-first-row">
+    <div class="footer-logo">
+        <a href="https://ppcrc.isquareit.edu.in">
+            <img src="image/logos/PPCRC.png">
+        </a>
+        <a href="https://isquareit.edu.in">
+            <img src="image/logos/i2it.png" alt="I2IT Logo">
+        </a>
+    </div>
+    <div class="footer-links">
+        <h3>Site Links</h3>
+        <ul>
+            <li><a href="about.php">About</a></li>
+            <li><a href="events.php">Events</a></li>
+            <li><a href="outreach-activities.php">Outreach Activities</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </div>
+    <div class="footer-links">
+        <h3>Center of Excellence</h3>
+        <ul>
+            <li><a href="Center-of-Excellence/Geoinformatics.php">Geoinformatics</a></li>
+            <li><a href="Center-of-Excellence/Cloud-Computing-Information-Security.php">Cloud
+                    Computing &
+                    Information Security</a></li>
+            <li><a href="Center-of-Excellence/Artificial-Intelligence.php">Artificial
+                    Intelligence</a></li>
+            <li><a href="Center-of-Excellence/Robotics-Automation.php">Robotics &
+                    Automation</a></li>
+        </ul>
+    </div>
+
+</div>
+
+<div class="footer-second-row">
+    <div class="copyright-info">
+        <span>Copyright &copy; PPCRC 2020.</span>
+    </div>
+    <div class="credits">
+        <span>Site designed and maintained by
+            <a href="https://www.linkedin.com/in/kritika-singh-7a90b51a1/">Kritika</a>, <a
+                href="https://www.linkedin.com/in/aayu-mangwani-984384176/">Aayu</a>, Aashutosh and
+            <a href="https://www.linkedin.com/in/nikkothari22/">Nikhil</a>.</span>
+    </div>
+
+
+</div>
+
+
+
+
+
+
+</footer>
+';
+?>
---
+++ header.php
@@ -0,0 +1,21 @@
+<?php echo '
+<header>
+        <div class="header-logo">
+            <a href="https://ppcrc.isquareit.edu.in">
+                <img src="image/logos/ppcrc-final-logo.png" alt="PPCRC Logo">
+            </a>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="about.html">About</a></li>
+                <li><a href="center-of-excellence.html">Center of Excellence</a></li>
+                <li><a href="events.html">Events</a></li>
+                <li><a href="outreach-activities.html">Outreach Activities</a></li>
+                <li><a href="contact.html">Contact</a></li>
+
+            </ul>
+        </nav>
+
+    </header>
+';
+?>
